88ebc663bf60501604fc7da88bb0970d226018bc
Comrak <jnardulli@hotmail.com>
2021-12-13 01:35:23 -0300

    adding on click event just for fun commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("keydown", (e) => {
-  const audio = document.querySelector(`audio[data-key=${e.key}`);
+  const audio = document.querySelector(`audio[data-key=${e.key}]`);
   const key = document.querySelector(`.key[data-key=${e.key}]`);
   const lastKey = document.querySelector("#LKEY");
   if (!audio) return;
@@ -8,6 +8,23 @@ window.addEventListener("keydown", (e) => {
   key.classList.add("playing");
   lastKey.innerHTML = `<h2 style="text-align:center; color:yellow">${e.key.toUpperCase()}</h2>`;
 });
+
+let teclas = document.getElementsByClassName("keys");
+for (const tecla of teclas) {
+  tecla.onclick = (e) => {
+    console.log(e.target.innerHTML);
+    const audio = document.querySelector(
+      `audio[data-key=${e.target.innerHTML.toLowerCase()}]`
+    );
+    const key = document.querySelector(
+      `.key[data-key=${e.target.innerHTML.toLowerCase()}]`
+    );
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+  };
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
